079917571afcc446e28be1a1def427aa9052ec51
youngdreamer33 <b92.a75@gmail.com>
2017-07-12 14:15:20 +0200

    Upload image and debug Symfony

--- Marmiwild/src/RecipesBundle/Form/RecipesType.php
+++ Marmiwild/src/RecipesBundle/Form/RecipesType.php
@@ -5,6 +5,8 @@ namespace RecipesBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
 
 class RecipesType extends AbstractType
 {
@@ -26,6 +28,13 @@ class RecipesType extends AbstractType
             ->add('quantities')
             ->add('type')
             ->add('keyWords')
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => false,
+                'download_label' => '...',
+                'download_uri' => true,
+                'image_uri' => true,
+            ]);
         ;
     }
     
--- mesNotes.md
+++ mesNotes.md
@@ -13,3 +13,5 @@ table recette du moment? Ou requête sql
 table recettes:id recette, nom, description, id chef, instruction, temps préparation, temps cuisson, difficulté, mots clés, id ingrédients, quantité, nombre de personne, note
 
 créer service qui ajoute directement en bdd les ingrédients et les différents mots clés.
+
+créer compte marmiton pour voir comment cela se présente
